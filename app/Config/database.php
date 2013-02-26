59858529d06c247bafad264c825dc117a777f5af
BOTOHELY <anissa.botohely@gmail.com>
2013-02-26 09:09:22 +0100

    adding config and new .gitignore

---
+++ app/Config/database.php
@@ -0,0 +1,14 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'port' => 8889,
+		'login' => 'root',
+		'password' => 'root',
+		'database' => 'cakebake',
+		'encoding' => 'UTF8'
+	);
+}
